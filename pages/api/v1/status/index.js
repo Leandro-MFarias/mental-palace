02663010b8c1c62378dbafe44bba24a73996d97c
Leandro <leandrorf1606@gmail.com>
2026-01-18 13:16:23 -0300

    finish `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,29 @@
 import database from "infra/database.js";
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json("Acima da média");
+  const updatedAt = new Date().toISOString();
+
+  const serverVersionResponse = await database.query(`SHOW server_version;`);
+  const serverVersionValue = serverVersionResponse.rows[0].server_version;
+
+  const maxConnectionsResponse = await database.query(`SHOW max_connections;`);
+  const maxConnectionsValue = maxConnectionsResponse.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnectionsResponse = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const openedConnectionsValue = openedConnectionsResponse.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: serverVersionValue,
+        max_connections: parseInt(maxConnectionsValue),
+        opened_connections: openedConnectionsValue,
+      },
+    },
+  });
 }
